f6e6824cfd45dfe08daf7a2eb0bf219d405c2a51
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2023-10-05 02:02:24 +0700

    UPDATE: InputAction new control

--- Assets/Scripts/PlayerScript/CharacterControlScript.cs
+++ Assets/Scripts/PlayerScript/CharacterControlScript.cs
@@ -9,18 +9,59 @@ public class CharacterControlScript : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
 
+    private PlayerControls _playerControl;
+
     private Vector2 moveDirection;
 
-    [SerializeField] private InputActionReference move, fireNormal, fireSpecial;
+    private InputAction move, fireNormal, fireSpecial;
+
+    //[SerializeField] private InputActionReference move, fireNormal, fireSpecial;
+
+    private void Awake()
+    {
+        _playerControl = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        move = _playerControl.Player.Move;
+        move.Enable();
+
+        fireNormal = _playerControl.Player.FireNormal;
+        fireNormal.Enable();
+        fireNormal.performed += FireNormalBullet;
+
+        fireSpecial = _playerControl.Player.FireSpecial;
+        fireSpecial.Enable();
+        fireSpecial.performed += FireSpecialBullet;
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        fireNormal.Disable();
+        fireSpecial.Disable();
+    }
 
     private void Update()
     {
-        moveDirection = move.action.ReadValue<Vector2>();
+        moveDirection = move.ReadValue<Vector2>();
+
     }
 
     private void FixedUpdate()
     {
         rigidbody.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private void FireNormalBullet(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire!!");
+    }
+
+    private void FireSpecialBullet(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire Special!!");
     }
 
 }
