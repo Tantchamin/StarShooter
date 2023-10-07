843246533eeb5166d5abd4e9202fb95bfc165812
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2023-10-07 22:32:32 +0700

    NEW: UI

--- Assets/Scripts/BackgroundScripts/BackgroundMoveScript.cs
+++ Assets/Scripts/BackgroundScripts/BackgroundMoveScript.cs
@@ -7,17 +7,12 @@ public class BackgroundMoveScript : MonoBehaviour
     [SerializeField] private float movePoint = 7.5f, endPoint = -7.5f;
     [SerializeField] private float moveSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(gameObject.transform.position.y);
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.position += new Vector3(0, -moveSpeed*Time.deltaTime, 0);
-        if(transform.position.y <= -endPoint)
+        if(transform.position.y <= endPoint)
         {
             gameObject.transform.position = new Vector3(0, movePoint, 0);
         }
