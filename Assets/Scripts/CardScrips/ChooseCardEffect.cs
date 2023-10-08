ef5fe56ed51b685fade886ae55e232465726a29c
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2023-10-09 01:12:48 +0700

    NEW: add sound, title scene

--- Assets/Scripts/CardScrips/ChooseCardEffect.cs
+++ Assets/Scripts/CardScrips/ChooseCardEffect.cs
@@ -6,27 +6,34 @@ public class ChooseCardEffect : MonoBehaviour
 {
     [SerializeField] private DisplayCard card;
     [SerializeField] private CardEffect cardEffect;
+    [SerializeField] private AudioSource audioSource;
 
     public void ChooseCard()
     {
         switch (card.card.effectType)
         {
             case Card.EffectType.Barrier:
+                audioSource.Play();
                 cardEffect.BarrierEffect();
                 break;
             case Card.EffectType.Bullet:
+                audioSource.Play();
                 cardEffect.BulletEffect();
                 break;
             case Card.EffectType.GlassCannon:
+                audioSource.Play();
                 cardEffect.GlassCannonEffect();
                 break;
             case Card.EffectType.GoldenMissile:
+                audioSource.Play();
                 cardEffect.GoldenMissileEffect();
                 break;
             case Card.EffectType.Missile:
+                audioSource.Play();
                 cardEffect.MissileEffect();
                 break;
             case Card.EffectType.Repair:
+                audioSource.Play();
                 cardEffect.RepairEffect();
                 break;
         }
