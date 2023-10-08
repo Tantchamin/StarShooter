c9f63b5f0b716b95419ba7dd076105277e3bb2a2
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2023-10-09 00:12:53 +0700

    UPDATE: Win and Lose UI

--- Assets/Scripts/UI_Scripts/WinLoseConditionScript.cs
+++ Assets/Scripts/UI_Scripts/WinLoseConditionScript.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class WinLoseConditionScript : MonoBehaviour
 {
+    [SerializeField] private GameObject winUI;
+    [SerializeField] private GameObject loseUI;
+    [SerializeField] private CharacterStatusScript _characterStatusScript;
+    [SerializeField] private CharacterControlScript _characterControlScript;
+    [SerializeField] private EnemyStatusScript _enemyStatusScript;
+    [SerializeField] private EnemyHealthStateScript _enemyHealthStateScript;
+    [SerializeField] private EnemyMoveScript _enemyMoveScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        winUI.SetActive(false);
+        loseUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_characterStatusScript.GetHealthPoint() <= 0)
+        {
+            loseUI.SetActive(true);
+            _characterControlScript.enabled = false;
+            //_enemyHealthStateScript.enabled = false;
+            //_enemyMoveScript.enabled = false;
+            Time.timeScale = 0;
+        }
+        else if(_enemyStatusScript.GetHealthPoint() <= 0)
+        {
+            loseUI.SetActive(true);
+            _characterControlScript.enabled = false;
+            Time.timeScale = 0;
+        }
+
     }
 }
